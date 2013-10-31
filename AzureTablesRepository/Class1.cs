75c256099f65851e7583543a89928b280ae76e74
Stephan Visagie <svisagie@gmail.com>
2013-10-31 09:48:12 +0200

    More projects

---
+++ AzureTablesRepository/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTablesRepository
+{
+    public class Class1
+    {
+    }
+}
---
+++ HosCommsApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HosCommsApi.WebApiApplication" Language="C#" %>
