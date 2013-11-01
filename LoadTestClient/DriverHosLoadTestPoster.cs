cbddb2bfb69f5c5c13091d5cc71c3f47225f7b92
Stephan Visagie <svisagie@gmail.com>
2013-11-01 16:09:04 +0200

    Add load test client

---
+++ LoadTestClient/DriverHosLoadTestPoster.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace LoadTestClient
+{
+	public class DriverHosLoadTestPoster
+	{
+		private readonly int _numberOfThreads;
+		private bool _stopping;
+		Task[] _tasks;
+
+		private static double _count = 0d;
+		private static double _prevCount = 0d;
+		private static double _duration = 0d;
+		private static readonly object DurationLock = new object();
+		private static readonly Stopwatch Stopwatch = new Stopwatch();
+		private static string _baseUrl;
+
+		public DriverHosLoadTestPoster(int numberOfThreads, string baseUrl)
+		{
+			_numberOfThreads = numberOfThreads;
+			_baseUrl = baseUrl;
+		}
+
+		public void Start()
+		{
+			_tasks = new Task[_numberOfThreads];
+			for (var i = 0; i < _numberOfThreads; i++)
+			{
+				var task = new Task(() => DoContinuousPost(i));
+				_tasks[i] = task;
+			}
+			_tasks.AsParallel().ForAll(t => t.Start());
+		}
+
+		public void Stop()
+		{
+			_stopping = true;
+			Task.WaitAll(_tasks);
+		}
+
+		public static double GetStats()
+		{
+			if (Stopwatch.IsRunning)
+			{
+				Stopwatch.Stop();
+				_duration = Stopwatch.Elapsed.TotalSeconds;
+				Stopwatch.Reset();
+			}
+			var result = _duration > 0 ? (_count - _prevCount) / _duration : 0;
+			_prevCount = _count;
+			Stopwatch.Start();
+			return result;
+		}
+
+		private void DoContinuousPost(int i)
+		{
+			var client = new RestClient(_baseUrl);
+			while (!_stopping)
+			{
+				var randomWorkStateChange = DriverWorkStateChange.GetRandomWorkStateChange();
+				//PostUsingHttpClient(randomWorkStateChange);
+				//PostUsingHttpWebRequest(randomWorkStateChange);
+				PostUsingRestSharp(randomWorkStateChange, client);
+			}
+		}
+
+		private async void PostUsingHttpClient(DriverWorkStateChange randomWorkStateChange)
+		{
+			using (var client = new HttpClient())
+			{
+				var uri = new Uri(_baseUrl + "/api/driver/" + randomWorkStateChange.DriverId + "/workstate");
+				try
+				{
+					var x = await client.PostAsJsonAsync(uri.ToString(), randomWorkStateChange);
+
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.InnerException.Message);
+				}
+				_count++;
+			}
+		}
+
+		private void PostUsingHttpWebRequest(DriverWorkStateChange randomWorkStateChange)
+		{
+			var httpWebRequest = (HttpWebRequest)WebRequest.Create(_baseUrl + "/api/driver/" + randomWorkStateChange.DriverId + "/workstate");
+			httpWebRequest.ContentType = "text/json";
+			httpWebRequest.Method = "POST";
+
+			using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+			{
+				var json = JsonConvert.SerializeObject(randomWorkStateChange);
+
+				streamWriter.Write(json);
+				streamWriter.Flush();
+				streamWriter.Close();
+
+				var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+				if (httpResponse.StatusCode == HttpStatusCode.OK)
+					_count++;
+
+			}
+		}
+
+		private static void PostUsingRestSharp(DriverWorkStateChange randomWorkStateChange, RestClient client)
+		{
+			var request = new RestRequest("api/driver/{driverId}/workstate", Method.POST);
+			request.AddUrlSegment("driverId", randomWorkStateChange.DriverId.ToString(CultureInfo.InvariantCulture));
+			request.AddBody(JsonConvert.SerializeObject(randomWorkStateChange));
+			try
+			{
+				client.ExecuteAsync(request, response =>
+				{
+					_count++;
+				});
+			}
+			catch (Exception ex)
+			{
+				Trace.WriteLine(GetExceptionDetails(ex));
+				throw;
+			}
+		}
+
+		private static string GetExceptionDetails(Exception exception)
+		{
+			return "Exception: " + exception.GetType()
+				 + "\r\nInnerException: " + exception.InnerException
+				 + "\r\nMessage: " + exception.Message
+				 + "\r\nStackTrace: " + exception.StackTrace;
+		}
+	}
+}
---
+++ LoadTestClient/DriverWorkStateChange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LoadTestClient
+{
+	public class DriverWorkStateChange
+	{
+		static readonly Random Rnd = new Random();
+
+		public int DriverId { get; set; }
+		public int WorkStateId { get; set; }
+		public DateTime Timestamp { get; set; }
+
+		public static DriverWorkStateChange GetRandomWorkStateChange(int maxDriverId = 999, int maxWorkStateId = 3)
+		{
+			return new DriverWorkStateChange
+			{
+				DriverId = Rnd.Next(0, 999),
+				WorkStateId = Rnd.Next(0, 3),
+				Timestamp = DateTime.UtcNow
+			};
+		}
+	}
+}
