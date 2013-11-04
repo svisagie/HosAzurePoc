490a03c661d4576bd83f30a4ef0a42a0d890ec1f
bfvanrooyen <barend.vanrooyen@mixtelematics.com>
2013-11-04 11:03:04 +0200

    Remove relay calls

--- HosUI/Controllers/DashboardController.cs
+++ HosUI/Controllers/DashboardController.cs
@@ -24,22 +24,25 @@ namespace HosUI.Controllers
             Uri serviceUri = ServiceBusEnvironment.CreateServiceUri("sb", CloudConfigurationManager.GetSetting("ServiceNamespace"), "DriverService");
             channelFactory = new ChannelFactory<IDriverChannel>("RelayEndpoint", new EndpointAddress(serviceUri));
             sharedSecretServicebusCredential.TokenProvider = TokenProvider.CreateSharedSecretTokenProvider(
-                CloudConfigurationManager.GetSetting("IssuerName"), 
+                CloudConfigurationManager.GetSetting("IssuerName"),
                 CloudConfigurationManager.GetSetting("IssuerSecret"));
             channelFactory.Endpoint.Behaviors.Add(sharedSecretServicebusCredential);
         }
 
 
-        private string GetDriverName(IDriverChannel Channel, int DriverId)
+        private string GetDriverName(int DriverId)
         {
-            try
+            using (IDriverChannel channel = channelFactory.CreateChannel())
             {
-                return Channel.GetDriver(DriverId).DriverName;
-            }
-            catch (Exception e)
-            {
-                //TODO: Write Trace error somewhere
-                //Trace. Console.WriteLine("Error: {0}", e.Message);
+                try
+                {
+                    return channel.GetDriver(DriverId).DriverName;
+                }
+                catch (Exception e)
+                {
+                    //TODO: Write Trace error somewhere
+                    //Trace. Console.WriteLine("Error: {0}", e.Message);
+                }
             }
             return "";
 
@@ -55,28 +58,27 @@ namespace HosUI.Controllers
             var dashboard = new Dashboard();
             var hosRepository = new HosRepository(CloudConfigurationManager.GetSetting("SqlDbConnectionString"));
 
-            using (IDriverChannel channel = channelFactory.CreateChannel())
-            {
-                dashboard.Workstates = hosRepository.FindLatestDriverWorkStates(30)
-                    .Select(dw => new Workstate
-                    {
-                        DriverId = dw.DriverId,
-                        DriverName = GetDriverName(channel, dw.DriverId),
-                        DriverWorkStateId = dw.DriverWorkStateId,
-                        Timestamp = dw.Timestamp.ToString("dd/MM/yyyy HH:mm:ss"),
-                        WorkStateId = dw.WorkStateId
-                    }).ToList();
 
-                dashboard.Summaries = hosRepository.FindDriverSummariesForDrivers(
-                    dashboard.Workstates.Select(dw => dw.DriverId))
-                    .Select(ds => new Summary
-                    {
-                        DriverId = ds.DriverId,
-                        DriverName = GetDriverName(channel, ds.DriverId),
-                        WorkStateId = ds.WorkStateId,
-                        TotalSeconds = ds.TotalSeconds
-                    });
-            }
+            dashboard.Workstates = hosRepository.FindLatestDriverWorkStates(30)
+                .Select(dw => new Workstate
+                {
+                    DriverId = dw.DriverId,
+                    DriverName = "",//GetDriverName(dw.DriverId),
+                    DriverWorkStateId = dw.DriverWorkStateId,
+                    Timestamp = dw.Timestamp.ToString("dd/MM/yyyy HH:mm:ss"),
+                    WorkStateId = dw.WorkStateId
+                }).ToList();
+
+            dashboard.Summaries = hosRepository.FindDriverSummariesForDrivers(
+                dashboard.Workstates.Select(dw => dw.DriverId))
+                .Select(ds => new Summary
+                {
+                    DriverId = ds.DriverId,
+                    DriverName = "",//DriverName = GetDriverName(ds.DriverId),
+                    WorkStateId = ds.WorkStateId,
+                    TotalSeconds = ds.TotalSeconds
+                });
+
             return View(dashboard);
         }
 
@@ -84,24 +86,28 @@ namespace HosUI.Controllers
         {
             var dashboard = new Dashboard();
             var hosRepository = new HosRepository(CloudConfigurationManager.GetSetting("SqlDbConnectionString"));
+
+
             dashboard.Workstates = hosRepository.FindLatestDriverWorkStates(30)
                 .Select(dw => new Workstate
                 {
                     DriverId = dw.DriverId,
-                    DriverName = "NA",
+                    DriverName = "",//DriverName = GetDriverName(dw.DriverId),
                     DriverWorkStateId = dw.DriverWorkStateId,
                     Timestamp = dw.Timestamp.ToString("dd/MM/yyyy HH:mm:ss"),
                     WorkStateId = dw.WorkStateId
                 }).ToList();
 
-            dashboard.Summaries = hosRepository.FindDriverSummariesForDrivers(dashboard.Workstates.Select(dw => dw.DriverId))
+            dashboard.Summaries = hosRepository.FindDriverSummariesForDrivers(
+                dashboard.Workstates.Select(dw => dw.DriverId))
                 .Select(ds => new Summary
                 {
                     DriverId = ds.DriverId,
-                    DriverName = "NA",
+                    DriverName = "",//DriverName = GetDriverName(ds.DriverId),
                     WorkStateId = ds.WorkStateId,
                     TotalSeconds = ds.TotalSeconds
                 });
+
 
             return Json(dashboard, JsonRequestBehavior.AllowGet);
         }
